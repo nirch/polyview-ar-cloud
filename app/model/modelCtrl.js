35d5897a85fbb50240eeca591e56cd921be62981
nirch <nirh2@yahoo.com>
2019-01-02 21:41:24 +0200

    start work on model page

--- app/model/modelCtrl.js
+++ app/model/modelCtrl.js
@@ -1,4 +1,13 @@
 
-app.controller('modelCtrl', function($scope) {
+app.controller('modelCtrl', function($scope, $routeParams, customerSrv, projectSrv, modelSrv) {
     $scope.page = "Model Page"
+
+    // Loading the model
+    customerSrv.getActive().then(customer => {
+        projectSrv.getByName(customer, $routeParams.projectName).then(project => {
+            modelSrv.getByName(project, $routeParams.modelName).then(model => {
+                $scope.model = model;
+            })
+        });
+    });
 })
--- app/project/projectCtrl.js
+++ app/project/projectCtrl.js
@@ -1,16 +1,17 @@
 
-app.controller('projectCtrl', function($scope, $routeParams, customerSrv, projectSrv, modelSrv) {
+app.controller('projectCtrl', function($scope, $routeParams, customerSrv, projectSrv, modelSrv, $location) {
 
+    // Loading the project and models
     customerSrv.getActive().then(customer => {
-        //$scope.customer = customer;
-
         projectSrv.getByName(customer, $routeParams.projectName).then(project => {
             $scope.project = project;
-
             modelSrv.getByProject(project).then(models => {
                 $scope.models = models;
             })
         });
     });
 
+    $scope.openModel = function(model) {
+        $location.path("/" +  $routeParams.projectName + "/" + model.techName);
+    }
 })

f480324a1cb6d24de058bbc5b88621f7f9ac56cd
nirch <nirh2@yahoo.com>
2019-05-21 14:15:20 +0300

    small refactor of poyviewer (moving into functions)

--- embed/app.js
+++ embed/app.js
@@ -43,155 +43,188 @@ app.controller("embedCtrl", function ($rootScope, $scope, $sce, $routeParams, mo
 
 
         if (model.gltfUrl) {
-            var loader = new THREE.GLTFLoader();
-
-            // Creating a perspective camera. 
-            // The arguments are sensible defaults. 
-            // If your model is very large you may have to adjust the last two arguments, 
-            // which are the frustum near plane and frustum far plane. 
-            // They basically state that nothing nearer than one unit and nothing farther than a thousand units will be rendered.
-            var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1000);
-
-            // A scene holds your camera(s), lights and models, as it would do in real life. 
-            // The code below adds a white ambient light which illuminates the whole scene without any shading.
-            var scene = new THREE.Scene();
-
-            var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-            renderer.physicallyCorrectLights = true;
-            renderer.gammaOutput = true;
-            renderer.gammaFactor = 2.2;
-            // renderer.setClearColor(0xffffff);
-            //renderer.setClearColor(new THREE.Color("hsl(0, 0%, 90%)"));
-            renderer.setPixelRatio(window.devicePixelRatio);
-            //this.renderer.setSize(el.clientWidth, el.clientHeight);
-            renderer.setSize(window.innerWidth, window.innerHeight);
-
-
-            var viewerElement = document.getElementById("viewer");
-            viewerElement.appendChild(renderer.domElement);
-
-            /* Controls */
-            var controls = new THREE.OrbitControls(camera, renderer.domElement);
-            controls.autoRotate = false;
-            controls.autoRotateSpeed = -10;
-            controls.screenSpacePanning = true;
-
-            // controls.enableDamping = true;
-            // controls.dampingFactor = 0.25;
-            // controls.enableZoom = true;
-            // var geometry = new THREE.BoxGeometry(1, 1, 1);
-            // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-            // var cube = new THREE.Mesh(geometry, material);
-            // scene.add(cube);
-
-            loader.load(model.gltfUrl, function (gltf) {
-
-                gltf.scene.updateMatrixWorld();
-                const box = new THREE.Box3().setFromObject(gltf.scene);
-                const size = box.getSize(new THREE.Vector3()).length();
-                const center = box.getCenter(new THREE.Vector3());
-
-                controls.reset();
-
-                gltf.scene.position.x += (gltf.scene.position.x - center.x);
-                gltf.scene.position.y += (gltf.scene.position.y - center.y);
-                gltf.scene.position.z += (gltf.scene.position.z - center.z);
-                controls.maxDistance = size * 10;
-                camera.near = size / 100;
-                camera.far = size * 100;
-                camera.updateProjectionMatrix();
-
-                camera.position.copy(center);
-                camera.position.x += size / 2.0;
-                camera.position.y += size / 5.0;
-                camera.position.z += size / 2.0;
-                camera.lookAt(center);
-
-                controls.enabled = true;
-                controls.saveState();
-
-                scene.add(gltf.scene);
-
-
-                var ambient = new THREE.AmbientLight(0xffffff, 1);
-                scene.add(ambient);
-
-                // const directional = new THREE.DirectionalLight(0xffffff, 0.8 * Math.PI);
-                // directional.position.set(0.5, 0, 0.866); // ~60º
-                // scene.add(directional);
-
-                const hemisphere = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1.85 );
-                scene.add( hemisphere );
-
-                renderer.toneMappingExposure = 1.0;
-
-
-                // Environment
-                const path = "../assets/skybox/"
-                const format = ".jpg"
-                const cubeMapURLs = [
-                    path + 'posx' + format, path + 'negx' + format,
-                    path + 'posy' + format, path + 'negy' + format,
-                    path + 'posz' + format, path + 'negz' + format
-                ];
-                const envMap = new THREE.CubeTextureLoader().load(cubeMapURLs);
-                envMap.format = THREE.RGBFormat;
-                traverseMaterials(gltf.scene, (material) => {
-                    if (material.isMeshStandardMaterial || material.isGLTFSpecularGlossinessMaterial) {
-                        material.envMap = envMap;
-                        material.needsUpdate = true;
-                    }
-                });
-
-
-
-                // traverseMaterials(gltf.scene, (material) => {
-                //     if (material.map) material.map.encoding = THREE.sRGBEncoding;
-                //     if (material.emissiveMap) material.emissiveMap.encoding = THREE.sRGBEncoding;
-                //     if (material.map || material.emissiveMap) material.needsUpdate = true;
-                // });
-
-                window.addEventListener('resize', resize, false);
-
-
-            }, undefined, function (error) {
-                console.error(error);
-            });
-
-
-            var animate = function () {
-                requestAnimationFrame(animate);
-
-                controls.update();
-                // cube.rotation.x += 0.01;
-                // cube.rotation.y += 0.01;
-
-                renderer.render(scene, camera);
-            };
-
-            animate();
-
-            var resize = function () {
-                const {clientHeight, clientWidth} = viewerElement.parentElement.parentElement;
-
-                camera.aspect = clientWidth / clientHeight;
-                camera.updateProjectionMatrix();
-                renderer.setSize(clientWidth, clientHeight);            
-            }
-
-            function traverseMaterials(object, callback) {
-                object.traverse((node) => {
-                    if (!node.isMesh) return;
-                    const materials = Array.isArray(node.material)
-                        ? node.material
-                        : [node.material];
-                    materials.forEach(callback);
-                });
-            }
-
+            initPolyviewer(model);
         }
 
     });
+
+    // ****** POLYVIEWER ******
+    // Needs to be moved to a directive...
+
+    function initPolyviewer(model) {
+        var loader = new THREE.GLTFLoader();
+
+        // Creating a perspective camera. 
+        // The arguments are sensible defaults. 
+        // If your model is very large you may have to adjust the last two arguments, 
+        // which are the frustum near plane and frustum far plane. 
+        // They basically state that nothing nearer than one unit and nothing farther than a thousand units will be rendered.
+        var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1000);
+
+        // A scene holds your camera(s), lights and models, as it would do in real life. 
+        // The code below adds a white ambient light which illuminates the whole scene without any shading.
+        var scene = new THREE.Scene();
+
+        // Initializing the renderer
+        var renderer = initRenderer();
+
+        // Creating and appending the canvas (renderer) to the DOM
+        var viewerElement = document.getElementById("viewer");
+        viewerElement.appendChild(renderer.domElement);
+
+        // Initializing the orbit controls
+        var controls = initControls(camera, renderer);
+
+        // Loading the model
+        loader.load(model.gltfUrl, function (gltf) {
+
+            // Setting the camera position and controls based on model dimensions
+            setCameraAndControlsBasedOnModel(gltf, controls, camera);
+
+            scene.add(gltf.scene);
+
+            setLighting(scene);
+
+            // MOVED TO initRenderer: renderer.toneMappingExposure = 1.0;
+
+            setEnvironment(traverseMaterials, gltf);
+
+            window.addEventListener('resize', resize, false);
+
+        }, undefined, function (error) {
+            console.error(error);
+        });
+
+
+        var animate = function () {
+            requestAnimationFrame(animate);
+
+            controls.update();
+            // cube.rotation.x += 0.01;
+            // cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+
+        var resize = function () {
+            const {clientHeight, clientWidth} = viewerElement.parentElement.parentElement;
+
+            camera.aspect = clientWidth / clientHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(clientWidth, clientHeight);            
+        }
+    }
+    
+    function initRenderer() {
+        var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        renderer.physicallyCorrectLights = true;
+        renderer.gammaOutput = true;
+        renderer.gammaFactor = 2.2;
+        // renderer.setClearColor(0xffffff);
+        //renderer.setClearColor(new THREE.Color("hsl(0, 0%, 90%)"));
+        renderer.setPixelRatio(window.devicePixelRatio);
+        //this.renderer.setSize(el.clientWidth, el.clientHeight);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.toneMappingExposure = 1.0;
+
+        return renderer;
+    }
+
+    function initControls(camera, renderer) {
+        var controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.autoRotate = false;
+        controls.autoRotateSpeed = -10;
+        controls.screenSpacePanning = true;
+        // controls.enableDamping = true;
+        // controls.dampingFactor = 0.25;
+        // controls.enableZoom = true;
+        return controls;
+    }
+
+    function setCameraAndControlsBasedOnModel(gltf, controls, camera) {
+        
+        // Getting model dimensions
+        gltf.scene.updateMatrixWorld();
+        const box = new THREE.Box3().setFromObject(gltf.scene);
+        const size = box.getSize(new THREE.Vector3()).length();
+        const center = box.getCenter(new THREE.Vector3());
+
+        controls.reset();
+
+        gltf.scene.position.x += (gltf.scene.position.x - center.x);
+        gltf.scene.position.y += (gltf.scene.position.y - center.y);
+        gltf.scene.position.z += (gltf.scene.position.z - center.z);
+
+        controls.maxDistance = size * 10;
+
+        // Updating the camera
+        camera.near = size / 100;
+        camera.far = size * 100;
+        camera.updateProjectionMatrix();
+        camera.position.copy(center);
+        camera.position.x += size / 2.0;
+        camera.position.y += size / 5.0;
+        camera.position.z += size / 2.0;
+        camera.lookAt(center);
+
+        // Updating the controls
+        controls.enabled = true;
+        controls.saveState();
+    }
+    
+    function setLighting(scene) {
+        var ambient = new THREE.AmbientLight(0xffffff, 1);
+        scene.add(ambient);
+
+        // const directional = new THREE.DirectionalLight(0xffffff, 0.8 * Math.PI);
+        // directional.position.set(0.5, 0, 0.866); // ~60º
+        // scene.add(directional);
+
+        const hemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff, 1.85);
+        scene.add(hemisphere);
+    }
+    
+    function setEnvironment(traverseMaterials, gltf) {
+        const path = "../assets/skybox/";
+        const format = ".jpg";
+        const cubeMapURLs = [
+            path + 'posx' + format, path + 'negx' + format,
+            path + 'posy' + format, path + 'negy' + format,
+            path + 'posz' + format, path + 'negz' + format
+        ];
+        const envMap = new THREE.CubeTextureLoader().load(cubeMapURLs);
+        envMap.format = THREE.RGBFormat;
+        traverseMaterials(gltf.scene, (material) => {
+            if (material.isMeshStandardMaterial || material.isGLTFSpecularGlossinessMaterial) {
+                material.envMap = envMap;
+                material.needsUpdate = true;
+            }
+        });
+
+        // traverseMaterials(gltf.scene, (material) => {
+        //     if (material.map) material.map.encoding = THREE.sRGBEncoding;
+        //     if (material.emissiveMap) material.emissiveMap.encoding = THREE.sRGBEncoding;
+        //     if (material.map || material.emissiveMap) material.needsUpdate = true;
+        // });
+
+    }
+
+    function traverseMaterials(object, callback) {
+        object.traverse((node) => {
+            if (!node.isMesh) return;
+            const materials = Array.isArray(node.material)
+                ? node.material
+                : [node.material];
+            materials.forEach(callback);
+        });
+    }
+
+    // ****** END POLYVIEWER ******
+
+
+    // ****** FULLSCREEN ******
 
     // Initially, not in full screen
     $scope.isInFullScreen = false;
@@ -212,6 +245,8 @@ app.controller("embedCtrl", function ($rootScope, $scope, $sce, $routeParams, mo
         $scope.isInFullScreen = isFullScreen;
         $scope.$apply();
     })
+
+    // ****** CHECK VIEWER ******
 
     var isAppleArQuickLook = false;
     var isClaraViewer = false;
@@ -259,3 +294,4 @@ app.controller("embedCtrl", function ($rootScope, $scope, $sce, $routeParams, mo
 
 
 });
+

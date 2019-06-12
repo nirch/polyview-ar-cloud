355232de048782e5b23248ffc0bebc9f931f3185
nirch <nirh2@yahoo.com>
2019-06-12 18:39:12 +0300

    admin: adding parse and dropdowns for project and model

--- admin/app/app.js
+++ admin/app/app.js
@@ -1,9 +1,27 @@
 
 var app = angular.module("adminApp", []);
 
-app.controller("adminCtrl", function($scope) {
+app.controller("adminCtrl", function($scope, customerSrv, projectSrv, modelSrv) {
 
     $scope.intensity = 1;
     $scope.bgColor = "#ffffff";
+    $scope.selectedProject = null;
+    $scope.selectedModel = null;
+
+    customerSrv.getActive().then(customer => {
+        projectSrv.getByCustomer(customer).then(projects => {
+            $scope.projects = projects;
+        });
+    });
+
+    $scope.onProjectSelected = function() {
+        modelSrv.getByProject($scope.selectedProject, false).then(models => {
+            $scope.models = models;
+        });
+    }
+
+    $scope.onModelSelected = function() {
+        alert($scope.selectedModel.displayName);
+    }
 
 });

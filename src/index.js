0a32d24ca3029ae16ae4414ff03bcd6df254562e
nirch <nirh2@yahoo.com>
2019-10-12 14:40:23 +0300

    fix sendfile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // loading env variables from .env
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
+const path = require('path')
 
 const app = express();
 app.use(cors());
@@ -10,8 +11,8 @@ app.use(require('prerender-node').set('prerenderToken', '5dXyDG2XnoirJXnU8ncs'))
 app.use(express.static("public")); 
 // app.use('/', express.static('public'));
 
-// app.get('*', function(req, res){ 
-//     res.sendFile('./public/index.html'); 
-//   });
+app.get('*', function(req, res){ 
+    res.sendFile(path.join(__dirname, '../public/index.html')); 
+});
 
 app.listen(process.env.PORT, () => console.log(`Polyview AR Cloud app listening on port ${process.env.PORT}!`))

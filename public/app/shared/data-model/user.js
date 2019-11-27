6dbc3fe6efa2629f5a96051e944ee869e132466b
nirch <nirh2@yahoo.com>
2019-11-27 15:26:56 +0200

    block login to connected customers only or admin

--- public/app/shared/data-model/user.js
+++ public/app/shared/data-model/user.js
@@ -1,11 +1,29 @@
 
-app.factory("userSrv", function () {
+app.factory("userSrv", function (customerSrv) {
 
     async function login(email, pwd) {
          // Pass the username and password to logIn function
          const user = await Parse.User.logIn(email, pwd);
-         console.log('Logged in user', user);
-         return user;
+         const customer = await customerSrv.getActive();
+         if (user.get("customer") && user.get("customer").id === customer.id) {
+            console.log('Logged in user', user);
+            return user;   
+         } else {
+            var queryRole = new Parse.Query(Parse.Role);
+            queryRole.equalTo('name', 'admin');
+            const adminRole = await queryRole.first();
+            var adminRelation = new Parse.Relation(adminRole, 'users');
+            var queryAdmins = adminRelation.query();
+            queryAdmins.equalTo('objectId', Parse.User.current().id);
+            var adminUser = await queryAdmins.first();
+
+            if (adminUser) {
+                return user;
+            } else {
+                await logout();
+                throw "User unauthorized";
+            }
+         }
     }
 
     async function logout() {
